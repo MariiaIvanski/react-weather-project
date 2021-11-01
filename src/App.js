b58c10423c990d8ddaa0a299e565009b17cdba6a
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-01 13:44:36 +0200

    import bootstrap and create weather component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a
-          href="https://inspiring-noyce-9d41a5.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mariia Ivanski
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MariiaIvanski/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project was coded by{" "}
+          <a
+            href="https://inspiring-noyce-9d41a5.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mariia Ivanski
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MariiaIvanski/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

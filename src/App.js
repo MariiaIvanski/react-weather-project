e0807e38d8d3bf24c2b4947c858f73c23939906d
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-01 10:29:24 +0200

    add GitHub repo and personal site url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a
+          href="https://inspiring-noyce-9d41a5.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mariia Ivanski
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MariiaIvanski/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

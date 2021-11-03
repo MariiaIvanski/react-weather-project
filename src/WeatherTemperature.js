42fa9f64551288db422b2dfb367f1fa4ce5c705d
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-03 22:24:12 +0200

    start work on unit conversion component

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">°C</span>
+      </div>
+    );
+  }
+}

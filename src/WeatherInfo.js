39843cdf3b05b9dc9224e1c470cc82b63d4ccfbb
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-03 17:54:57 +0200

    create icon component and connect package images to api images

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,11 +16,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="d-flex justify-content-start">
             <WeatherIcon code={props.data.icon} />
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+
             <div className="float-left">
               <span className="temperature">
                 {Math.round(props.data.temperature)}

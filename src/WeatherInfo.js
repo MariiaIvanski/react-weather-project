f8d82c8970743f5bb829eaecc1a32ec99ef6d63e
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-05 19:23:15 +0200

    create forecast layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex justify-content-start">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={64} />
 
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />

f6002ce90ed0d99fac138c4e5d01cd0e85f9ba8d
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-07 14:23:56 +0200

    imported location information to WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,12 +3,12 @@ import WeatherIcaon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   function handleResponse(response) {}
   const apiKey = "a2d283df905dedf8786b96ad24673f92";
-  let longitude = 40.7;
-  let latitude = 74;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}$units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
 

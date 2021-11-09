aab7bea7b92824d605cf479c443b1eea8167b2d6
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-09 21:01:43 +0200

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,5 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals();
